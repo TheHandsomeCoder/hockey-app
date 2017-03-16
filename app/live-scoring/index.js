1144f2d36c67c0afd4094635ae858a120678eaa4
Scott O'Malley <scottyomalley@gmail.com>
2017-03-16 00:15:44 +0000

    Fixed module names

--- app/live-scoring/index.js
+++ app/live-scoring/index.js
@@ -4,7 +4,7 @@ import uirouter from 'angular-ui-router';
 import routes from './config/live-scoring.route.config';
 import LiveScoringController from './live-scoring.controller';
 
-export default angular.module('app.home', [uirouter])
+export default angular.module('app.liveScoring', [uirouter])
     .config(routes)
     .controller('LiveScoringController', LiveScoringController)
     .name;  //export the name of the module so we can register it the parent app
--- app/team-builder/index.js
+++ app/team-builder/index.js
@@ -4,7 +4,7 @@ import uirouter from 'angular-ui-router';
 import routes from './config/team-builder.route.config';
 import TeamBuilderController from './team-builder.controller';
 
-export default angular.module('app.team-builder', [uirouter])
+export default angular.module('app.teamBuilder', [uirouter])
     .config(routes)
     .controller('TeamBuilderController', TeamBuilderController)
     .name;  //export the name of the module so we can register it the parent app
